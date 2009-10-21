f1641b9be2f28abae2110f16de9045312ba054fc
dpt <none@none>
2009-10-21 16:39:31 +0100

    Set permissions before uploading.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# set permissions
+#
+find . \( -type d -not -name CVS \) | xargs chmod 755
+find . \( -type d -name CVS -prune \) -or \( -type f -print \) | xargs chmod 644
+
 # -C        auto-ignore files in the same way CVS does
 # -r        recurse
 # -t        preserve times
@@ -8,7 +13,7 @@
 #
 # --delete  is only required when deleting anything which doesn't exist on
 #           the server
-
+#
 rsync -Crtvz --exclude="Thumbs.db" . octopus.pepperfish.net:websites/davespace.co.uk/www/html
 
 exit 0
